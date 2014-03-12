e2f27f3c5076f2833990a93d1506e51b9b3c6f24
fb55 <me@feedic.com>
2014-03-12 14:32:53 +0100

    removed unused variables

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ module.exports = parse;
 
 var re_ws = /^\s/,
     re_name = /^(?:\\.|[\w\-\u00c0-\uFFFF])+/,
-    re_combinators = /^\s*[^\\]\s*[>~+,]|$\s*/g,
     re_escape = /\\([\da-f]{1,6}\s?|(\s)|.)/ig,
-    re_comma = /^\s*,\s*/,
     //modified version of https://github.com/jquery/sizzle/blob/master/src/sizzle.js#L87
     re_attr = /^\s*((?:\\.|[\w\u00c0-\uFFFF\-])+)\s*(?:(\S?)=\s*(?:(['"])(.*?)\3|(#?(?:\\.|[\w\u00c0-\uFFFF\-])*)|)|)\s*(i)?\]/;
 
