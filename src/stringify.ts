f0b6e84fb8bdbd636fd1609ebe7c9602be4f3ced
Felix <188768+fb55@users.noreply.github.com>
2026-02-22 10:23:24 +0000

    fix(lint): address review follow-ups

--- src/stringify.ts
+++ src/stringify.ts
@@ -140,7 +140,9 @@ function stringifyToken(
     }
 }
 
-function getActionValue(action: AttributeAction): string {
+function getActionValue(
+    action: Exclude<AttributeAction, AttributeAction.Exists>,
+): string {
     switch (action) {
         case AttributeAction.Equals: {
             return "";
@@ -162,9 +164,6 @@ function getActionValue(action: AttributeAction): string {
         }
         case AttributeAction.Hyphen: {
             return "|";
-        }
-        case AttributeAction.Exists: {
-            return "";
         }
         default: {
             throw new Error("Shouldn't be here");
